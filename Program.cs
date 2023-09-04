8fe7dd680a226ef47f457971a895b16e8799a58a
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-09-04 14:25:40 +0200

    Getting started with defining and creating a simple method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HelloWorld();
+            Console.ReadLine();
+        }
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello world!");
         }
     }
 }
